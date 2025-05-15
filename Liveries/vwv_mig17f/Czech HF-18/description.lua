ac0a4fe0d89d046e795f7509bbb32bdab2e8b953
TeTeT Nimitz <tetet.nimitz@gmail.com>
2025-05-15 06:46:58 +0200

    Major refactor, remove flyability

--- Liveries/vwv_mig17f/Czech HF-18/description.lua
+++ Liveries/vwv_mig17f/Czech HF-18/description.lua
@@ -1,5 +1,5 @@
 livery = {
-	{"body_main",	   	0, "MIG-17PM_Czec.dds" ,false},\
+	{"body_main",	   	0, "MIG-17PM_Czec.dds" ,false},
 
 	{"body_main",		ROUGHNESS_METALLIC			,"MIG-17 TEST RM5"	, true}, 
 	{"Material #20",	ROUGHNESS_METALLIC,	"MIG-17 TEST RM5", true},
